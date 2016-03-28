a9f1c14af4ea29febcf41005c5a7b8a62aa68b56
Cesar Forero <cesar.forero.gz@gmail.com>
2016-03-28 01:22:02 -0500

    remove comments

--- service.js
+++ service.js
@@ -1,15 +1,9 @@
 var amqp = require('amqplib/callback_api');
 var mqtt    = require('mqtt');
-//var mqtt = require('mqtt')
-//  , host = 'localhost' // or localhost
-//  , client = mqtt.connect();
 
 var client  = mqtt.connect('mqtt://localhost');
-//var client = mqtt.createClient(1883, 'localhost');
-//var client = mqtt.connect(options);
 var channel = null
   console.log(" Connecting to rabbit");
-  //ec2-52-91-34-111.compute-1.amazonaws.com:5672
 amqp.connect('amqp://localhost', function(err, conn) {
      conn.createChannel(function(err, ch) {
         channel = ch
